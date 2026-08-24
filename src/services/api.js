e96374fed12e87fefee885c92108d03c9a14283f
joana1818 <leal.j@acad.ifma.edu.br>
2026-08-24 18:42:28 -0300

    Fix API URL para Render

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = import.meta.env.VITE_API_URL || (import.meta.env.DEV ? '/api' : 'https://cinecocaisback.onrender.com/api')
+const baseUrl = 'https://cinecocaisback.onrender.com/api'
 
 const api = axios.create({
   baseURL: baseUrl,
